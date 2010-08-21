ce104871b02a86be4ddfbaf8e86cadcd5483f89a
Michel Beloshitsky <mbeloshitsky@gmail.com>
2010-08-21 22:18:12 +0600

    Added processing <pre> elements.

--- src/fittowidth.js
+++ src/fittowidth.js
@@ -5,31 +5,52 @@
    var port = chrome.extension.connect({name: "ftw32"})
    var isFit = localStorage.getItem('fitToWidth32764')
 
+   /* For */
+   var preWrapObj
+
+   function addPreWrapCSS() {
+      var preWrapElt = document.createElement("style");
+      preWrapElt.type = "text/css";
+      preWrapElt.appendChild(
+	 document.createTextNode('pre { white-space: pre-wrap; }')
+      );
+      return preWrapElt
+   }
+
    function iter(f) {
       a = document.createNodeIterator(document, NodeFilter.SHOW_ELEMENT, f, false)
       while (a.nextNode()) {}
    }
 
    function fit(isFit) {
+
+      /* Wrap pre elements */
+      if (isFit) {
+	 preWrapObj = document.querySelector('head').appendChild(addPreWrapCSS())
+      } else {
+	 preWrapObj.parentNode.removeChild(preWrapObj)
+      }
+
+      /* Walk on DOM items and adjust it */
       iter(isFit ? function (e) {
-	 var realLeft = e.offsetLeft, i = e
-	 
-	 /* This is potential bottleneck.
-	  
-	    Solution is do treewalk instead of nodeIterator, 
-	    but it's too lazy for now. */
-	 while (i.offsetParent) {
-	    i = i.offsetParent
-	    realLeft += i.offsetLeft
-	 }
-	 
+         var realLeft = e.offsetLeft, i = e
+
+         /* This is potential bottleneck.
+
+            Solution is do treewalk instead of nodeIterator,
+            but it's too lazy for now. */
+         while (i.offsetParent) {
+            i = i.offsetParent
+            realLeft += i.offsetLeft
+         }
+
          var maxW = document.documentElement.clientWidth - realLeft - SCROLLBAR_ADJUST
-	 
-	 if (maxW < e.offsetWidth) {
+
+         if (maxW < e.offsetWidth) {
             if(e.tagName == 'IMG')
                e.style.maxHeight = e.clientHeight * maxW / e.clientWidth
             e.style.maxWidth = maxW + 'px'
-	 }
+         }
       } : function (e) { if (e.style.maxWidth) e.style.maxWidth = '' })
    }
 
@@ -41,8 +62,8 @@
 
    function processMessage (msg) {
       if (msg.cmd == 'toggle') {
-	 isFit = !isFit
-	 applyFit();
+         isFit = !isFit
+         applyFit();
       }
    }
 
